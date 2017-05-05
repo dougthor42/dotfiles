9eabfc38e15f8d3c37ced31d0e2ec2aed90ac8cc
Douglas Thor <doug.thor@gmail.com>
2017-05-05 16:32:52 -0700

    Added empty .tmux.conf file

--- mklinks.sh
+++ mklinks.sh
@@ -15,6 +15,7 @@ rm "/home/$USER/.lessfilter"
 rm "/home/$USER/.profile"
 rm -r "/home/$USER/.vim"
 rm "/home/$USER/.toprc"
+rm "/home/$USER/.tmux.conf"
 
 echo "Creating symlinks for dotfiles."
 ln -s "/home/$USER/dotfiles/.bashrc" "/home/$USER/.bashrc"
@@ -24,6 +25,7 @@ ln -s "/home/$USER/dotfiles/.lessfilter" "/home/$USER/.lessfilter"
 ln -s "/home/$USER/dotfiles/.profile" "/home/$USER/.profile"
 ln -s "/home/$USER/dotfiles/.vim" "/home/$USER/.vim"
 ln -s "/home/$USER/dotfiles/.toprc" "/home/$USER/.toprc"
+ln -s "/home/$USER/dotfiles/.tmux.conf" "/home/$USER/.tmux.conf"
 
 echo "Complete."
 
