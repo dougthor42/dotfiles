a4a608eb189c64f3a395f7beb000bd39a3282bc9
Douglas Thor <doug.thor@gmail.com>
2022-05-19 23:37:18 +0000

    Update mklinks.sh to make it easier to support other home dirs

--- mklinks.sh
+++ mklinks.sh
@@ -10,28 +10,32 @@ curdir=$(pwd)
 # Change to the home dir. This isn't really needed.
 cd ~
 
+# Add support for different home directories.
+homedir="/home/$USER"
+# homedir="/usr/local/google/home/dthor"
+
 echo "Deleting old dotfiles."
-rm "/home/$USER/.bashrc"
-rm "/home/$USER/.bash_logout"
-rm "/home/$USER/.gitconfig"
-rm "/home/$USER/.lessfilter"
-rm "/home/$USER/.profile"
-rm -r "/home/$USER/.vim"
-rm "/home/$USER/.toprc"
-rm "/home/$USER/.tmux.conf"
+rm "$homedir/.bashrc"
+rm "$homedir/.bash_logout"
+rm "$homedir/.gitconfig"
+rm "$homedir/.lessfilter"
+rm "$homedir/.profile"
+rm -r "$homedir/.vim"
+rm "$homedir/.toprc"
+rm "$homedir/.tmux.conf"
 
 echo "Creating symlinks for dotfiles."
-ln -s "/home/$USER/dotfiles/.bashrc" "/home/$USER/.bashrc"
-ln -s "/home/$USER/dotfiles/.bash_logout" "/home/$USER/.bash_logout"
-ln -s "/home/$USER/dotfiles/.gitconfig" "/home/$USER/.gitconfig"
-ln -s "/home/$USER/dotfiles/.lessfilter" "/home/$USER/.lessfilter"
-ln -s "/home/$USER/dotfiles/.profile" "/home/$USER/.profile"
-ln -s "/home/$USER/dotfiles/.vim" "/home/$USER/.vim"
-ln -s "/home/$USER/dotfiles/.toprc" "/home/$USER/.toprc"
-ln -s "/home/$USER/dotfiles/.tmux.conf" "/home/$USER/.tmux.conf"
+ln -s "$homedir/dotfiles/.bashrc" "$homedir/.bashrc"
+ln -s "$homedir/dotfiles/.bash_logout" "$homedir/.bash_logout"
+ln -s "$homedir/dotfiles/.gitconfig" "$homedir/.gitconfig"
+ln -s "$homedir/dotfiles/.lessfilter" "$homedir/.lessfilter"
+ln -s "$homedir/dotfiles/.profile" "$homedir/.profile"
+ln -s "$homedir/dotfiles/.vim" "$homedir/.vim"
+ln -s "$homedir/dotfiles/.toprc" "$homedir/.toprc"
+ln -s "$homedir/dotfiles/.tmux.conf" "$homedir/.tmux.conf"
 
-echo "Applying .bashrc"
-source "/home/$USER/.bashrc"
+echo "Applying $homedir/.bashrc"
+source "$homedir/.bashrc"
 
 # Make sure we move back to the dir the user was in.
 cd "$curdir"
