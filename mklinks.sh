576b13389f6a1cafb5eb90def23ac3a9ffb14320
Douglas Thor <doug.thor@gmail.com>
2022-08-22 16:19:02 -0700

    Use $HOME instead of trying to manually support multiple home dirs. Dummy.

--- mklinks.sh
+++ mklinks.sh
@@ -13,37 +13,32 @@ curdir=$(pwd)
 # Change to the home dir. This isn't really needed.
 cd ~ || return
 
-# Add support for different home directories.
-homedir="/home/$USER"
-# homedir="/usr/local/google/home/dthor"
-
 echo "Deleting old dotfiles."
-rm "$homedir/.bashrc"
-rm "$homedir/.bash_logout"
-rm "$homedir/.gitconfig"
-rm "$homedir/.lessfilter"
-rm "$homedir/.profile"
-rm -r "$homedir/.vim"
-rm "$homedir/.toprc"
-rm "$homedir/.tmux.conf"
-rm "$homedir/.gnupg/gpg-agent.conf"
+rm "$HOME/.bashrc"
+rm "$HOME/.bash_logout"
+rm "$HOME/.gitconfig"
+rm "$HOME/.lessfilter"
+rm "$HOME/.profile"
+rm -r "$HOME/.vim"
+rm "$HOME/.toprc"
+rm "$HOME/.tmux.conf"
+rm "$HOME/.gnupg/gpg-agent.conf"
 
 echo "Creating symlinks for dotfiles."
-ln -s "$homedir/dotfiles/.bashrc" "$homedir/.bashrc"
-ln -s "$homedir/dotfiles/.bash_logout" "$homedir/.bash_logout"
-ln -s "$homedir/dotfiles/.gitconfig" "$homedir/.gitconfig"
-ln -s "$homedir/dotfiles/.lessfilter" "$homedir/.lessfilter"
-ln -s "$homedir/dotfiles/.profile" "$homedir/.profile"
-ln -s "$homedir/dotfiles/.vim" "$homedir/.vim"
-ln -s "$homedir/dotfiles/.toprc" "$homedir/.toprc"
-ln -s "$homedir/dotfiles/.tmux.conf" "$homedir/.tmux.conf"
-ln -s "$homedir/dotfiles/.gnupg/gpg-agent.conf" "$homedir/.gnupg/gpg-agent.conf"
+ln -s "$HOME/dotfiles/.bashrc" "$HOME/.bashrc"
+ln -s "$HOME/dotfiles/.bash_logout" "$HOME/.bash_logout"
+ln -s "$HOME/dotfiles/.gitconfig" "$HOME/.gitconfig"
+ln -s "$HOME/dotfiles/.lessfilter" "$HOME/.lessfilter"
+ln -s "$HOME/dotfiles/.profile" "$HOME/.profile"
+ln -s "$HOME/dotfiles/.vim" "$HOME/.vim"
+ln -s "$HOME/dotfiles/.toprc" "$HOME/.toprc"
+ln -s "$HOME/dotfiles/.tmux.conf" "$HOME/.tmux.conf"
+ln -s "$HOME/dotfiles/.gnupg/gpg-agent.conf" "$HOME/.gnupg/gpg-agent.conf"
 
-echo "Applying $homedir/.bashrc"
-source "$homedir/.bashrc"
+echo "Applying $HOME/.bashrc"
+source "$HOME/.bashrc"
 
 # Make sure we move back to the dir the user was in.
 cd "$curdir" || return
 
 echo "Complete."
-
