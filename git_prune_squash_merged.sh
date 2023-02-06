fda3590415a9e25858c56d1c3aa526179ceeb15d
Douglas Thor <doug.thor@gmail.com>
2023-02-06 20:46:38 +0000

    Update git_prune_squash_merged.sh:
    + Add a '-v/--verbose' option
    + Do not delete branches that are the base of other branches
    + Logic cleanup.

--- git_prune_squash_merged.sh
+++ git_prune_squash_merged.sh
@@ -13,10 +13,15 @@
 # Parse args
 # See https://stackoverflow.com/a/33826763/1354930
 DRY_RUN=0
+VERBOSE=0
+
+MAIN_BRANCH="master"
+
 while [[ "$#" -gt 0 ]];
 do
 	case $1 in
 		-n|--dry-run) DRY_RUN=1; shift ;;
+		-v|--verbose) VERBOSE=1; shift ;;
 		*) echo "Unknown parameter passed: $1"; exit 1 ;;
 	esac
 	shift
@@ -33,12 +38,70 @@ fi;
 git checkout -q master &&
 git for-each-ref refs/heads/ "--format=%(refname:short)" |
 while read branch;
-do mergeBase=$(git merge-base master "$branch") &&
-	[[ $(git cherry master $(git commit-tree $(git rev-parse "$branch^{tree}") -p "$mergeBase" -m _)) == "-"* ]] &&
+do
+	# Get the merge-base. The merge-base is the "best common ancestor(s)
+	# between two commits". Eg: the commit on master that a branch was
+	# created from.
+	mergeBase=$(git merge-base master "$branch");
+
+	# Default case: we don't delete anything.
+	WILL_DELETE=0;
+
+	# Test if that branch has been squash-merged onto master.
+	# This is black magic to me.
+	if [[ $(git cherry master $(git commit-tree $(git rev-parse "$branch^{tree}") -p "$mergeBase" -m _)) == "-"* ]]
+	then
+		# Technically we don't need to set this here, but I like having it.
+		WILL_DELETE=1;
+	else
+		# Branch isn't squash-merged, we con't need to check anything else
+		continue
+	fi;
+
+	# If we got down here then we already know that $branch is merged into
+	# master. We still want to check if it's a dependency of other branches.
+
+
+	# Is the branch included in any other branch?
+	# `--contains` will always show itself, so remove that line.
+	# |$ git branch --contains master
+	# |  apt-app-factory-save
+	# |* master
+	# |  u/dthor/29267-apt-app-factory
+	# becomes
+	# |$ git branch --contains master
+	# |  apt-app-factory-save
+	# |  u/dthor/29267-apt-app-factory
+	CONTAINS=$(git branch --contains "$branch" | sed -e "\:^[\* ] $branch$:d");
+
+	# Is $CONTAINS empty?
+	if [[ -z $CONTAINS ]]
+	then
+		# If so, it's safe to delete this branch.
+		WILL_DELETE=1
+		if [[ $VERBOSE -eq 1 ]]
+		then
+			echo "$branch is not a dependency on any other branch. Deleting";
+		fi;
+	else
+		# If not, we only delete if --force is given. (TODO)
+		echo "$branch is a dependency of other branch(es). Not deleting without --force"
+		if [[ $VERBOSE -eq 1 ]]
+		then
+			echo "$CONTAINS";
+		fi;
+		continue
+	fi;
+
 	if [[ $DRY_RUN -eq 1 ]]
 	then
+		WILL_DELETE=0
 		echo "$branch is merged into master and can be deleted";
-	else
-		git branch -D "$branch";
 	fi;
+
+	if [[ $WILL_DELETE -eq 1 ]]
+	then
+		git branch -D "$branch"
+	fi;
+
 done
